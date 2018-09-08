304ccdee9113d1d4cb16312764659be3916eca81
Samuel Figueroa-Lazu <sammylazu@gmail.com>
2018-09-08 14:24:41 -0500

    Removed Printers route

--- src/components/PrivateRoutes.js
+++ src/components/PrivateRoutes.js
@@ -7,7 +7,6 @@ import Assets from './Assets';
 import AssetForm from './AssetForm';
 import AssetInfo from './AssetInfo';
 import GetAsset from './queries/GetAsset';
-import Printers from './Printers';
 
 const PrivateRoutes = ({ auth }) => {
   const {user, isAuthenticated} = auth;
@@ -23,7 +22,6 @@ const PrivateRoutes = ({ auth }) => {
           </GetAsset>
         )}/>
       </Switch>
-      <Route exact path="/printers" render={()=><Printers />} />
       {
         user.admin ? (
           <Route exact path="/register" component={Register} />
