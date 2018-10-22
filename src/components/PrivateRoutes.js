de89e0ecc3bb3c28124297f049a12301170f95bc
Samuel Figueroa-Lazu <sammylazu@gmail.com>
2018-10-22 09:39:48 -0500

    Added hash routes to tab components and asset search route.

--- src/components/PrivateRoutes.js
+++ src/components/PrivateRoutes.js
@@ -1,15 +1,18 @@
 import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import queryString from 'query-string';
 import PropTypes from 'prop-types';
 import Register from './Register';
-
 import Assets from './Assets';
+import AssetTabs from './AssetTabs';
 import EquipmentForm from './EquipmentForm';
 import SupplyForm from './SupplyForm';
 import EquipmentInfo from './EquipmentInfo';
 import SupplyInfo from './SupplyInfo';
 import GetAsset from './queries/GetAsset';
+import GetAssets from './queries/GetAssets';
 import Printers from './Printers';
+import AssetSearch from './AssetSearch';
 
 const assetForms = {
   equipment: EquipmentForm,
@@ -25,17 +28,33 @@ const PrivateRoutes = ({ auth }) => {
   const {user, isAuthenticated} = auth;
   return (
     <Fragment>
-      <Route exact path="/assets" render={()=><Assets />} />
+      <Route exact path="/assets" component={Assets}/>
       <Switch>
+        <Route exact path="/assets/all" render={({ location })=><AssetTabs category={location.hash.slice(1)}/>}/>
+        <Route exact path="/assets/search" render={({ location })=>(
+          <GetAssets>
+            {
+              (getAssets, errors, clearErrors) =>
+                <AssetSearch
+                  data={{
+                    query: getAssets,
+                    errors,
+                    clearErrors
+                  }}
+                  search={queryString.parse(location.search).q}
+                />
+            }
+          </GetAssets>
+        )} />
         <Route exact path="/assets/:category/new" render={
           ({ match }) => {
             let AssetForm = assetForms[match.params.category];
             return <AssetForm user={user} />;
           }} />
         <Route exact path="/assets/:category/:id" render={
-          ({ match }) => {
+          ({ match, location }) => {
             let AssetInfo = assetInfo[match.params.category];
-            return <AssetInfo user={user} isAuthenticated={isAuthenticated} id={match.params.id}/>;
+            return <AssetInfo user={user} isAuthenticated={isAuthenticated} id={match.params.id} section={location.hash.slice(1)}/>;
           }} />
         <Route exact path="/assets/:category/:id/update" render={({ match }) => (
           <GetAsset id={match.params.id}>
