88a6edf035becb7ee1b071135ccee6bfd0e7333e
Samuel Figueroa-Lazu <sammylazu@gmail.com>
2019-08-01 22:53:12 -0500

    Added linter rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
     "linebreak-style": ["error","unix"],
     "quotes": ["error","single"],
     "semi": ["error","always"],
-    "no-console": ["warn", { "allow": ["info", "error"] }]
+    "no-console": ["warn", { "allow": ["info", "error"] }],
+    "no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": true }]
   }
 };
